b1aef9f73f205d8f1dacd5b60390a4559068592f
LUIS FERNANDO ESPINOSA HERRERA <ing.luiferch@gmail.com>
2022-12-04 22:11:12 -0500

    commig

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,33 @@ import { Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
 import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
+import useSWR from 'swr'
+
+
+const fetcher = (...args:[key:string]) => fetch(...args).then(res => res.json())
 
 
 const Home: NextPage = () => {
+
+
+    const { data, error } = useSWR('/api/products', fetcher,{
+      refreshInterval: 1000,
+    })
+  
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+  
+    
+    console.log(data)
+
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
         <ProductList 
-          products={ initialData.products as any }
+          products={data}
         />
     
 
